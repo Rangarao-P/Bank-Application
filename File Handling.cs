1a3b75d795fd6c168504d215c38b18039a57b20d
Rangarao-P <99806339+Rangarao-P@users.noreply.github.com>
2022-02-18 10:58:45 +0530

    commit

--- File Handling.cs
+++ File Handling.cs
@@ -8,5 +8,27 @@ namespace Bank_application
 {
     class File_Handling
     {
+        public void data()
+        {
+            string filepath = "C:\\userdel.txt";
+            try
+            {
+
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.Write(Environment.NewLine + "Appending this new line");
+                    sw.Close();
+                }
+
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine(File.ReadAllText(filepath));
+            }
+        }
     }
 }
